7e358227f1c09209276c0b6f3858e758fcd94981
Marco Frigeni <119053214+M4rga@users.noreply.github.com>
2025-01-14 12:26:24 +0100

    welcome page

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,30 @@
-import { View, Text } from "react-native";
-import { Link } from "expo-router";
-import React from "react";
+  import React from "react";
+  import { StyleSheet, ImageBackground, Image, View } from "react-native";
+  import { Link } from "expo-router";
 
-const index = () => {
-  return (
-    <View style={{ marginTop: 100 }}>
-      <Link href="/(tabs)">
-        <Text>Website + barca</Text>
-      </Link>
-    </View>
-  );
-};
+  const index = () => {
+    return (
+      <ImageBackground
+        source={require("../assets/images/going merry.png")}
+        style={{ flex: 1 }}
+      >
+        <Image 
+          source={require("../assets/images/scritta.png")} 
+          style={styles.scritta} 
+        />
+        <Link href={"/(tabs)"} replace style={{position: "absolute", width: "100%", height: "100%"}}></Link>
+      </ImageBackground>
+    );
+  };
 
-export default index;
+  const styles = StyleSheet.create({
+    scritta: {
+      resizeMode: "contain",
+      width: "80%",
+      position: "absolute",
+      left: "10%",
+      bottom: "25%",
+    },
+  });
+
+  export default index;
