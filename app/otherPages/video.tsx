2759a0cee10025147b94b9af49bfc375273a6bd2
Marco Frigeni <119053214+M4rga@users.noreply.github.com>
2025-01-15 15:33:39 +0100

    working video

--- app/otherPages/video.tsx
+++ app/otherPages/video.tsx
@@ -1,25 +1,47 @@
-import { View, Text, StyleSheet } from "react-native";
-import React from 'react'
-import { useLocalSearchParams } from "expo-router";
+import { View, StyleSheet, Text } from "react-native";
+import React, { useRef } from 'react';
+import { useLocalSearchParams, useRouter  } from "expo-router";
+import { Video, VideoFullscreenUpdateEvent } from 'expo-av';
 
-const video = () => {
+const VideoPlayer = () => {
+  const router = useRouter();
+  const videoRef = useRef<Video>(null);
   const params = useLocalSearchParams();
-  const { id = 42, other } = params;
+  const { id = null } = params;
+
+  const handleEnterFullscreen = () => {
+    if (videoRef.current) {
+      videoRef.current.presentFullscreenPlayer();
+    }
+  };
+
+  const handleFullscreenUpdate = (status: VideoFullscreenUpdateEvent) => {
+    if (status.fullscreenUpdate == 2 ) {
+      router.back()
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>
-        Qui andrà l'episodio: {id}
-      </Text>
+      <Video
+        ref={videoRef}
+        source={{ uri: 'https://pixeldrain.com/api/file/WU4Vk7Sn' }}
+        useNativeControls={true}
+        shouldPlay={true}
+        onLoad={handleEnterFullscreen}
+        onFullscreenUpdate={handleFullscreenUpdate}
+      />
+      <Text>Loading...</Text>
     </View>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
-  container: { 
-    flex: 1, 
+  container: {
+    flex: 1,
     alignItems: "center",
     justifyContent: "center",
-  },
+  }
 });
 
-export default video
+export default VideoPlayer;
